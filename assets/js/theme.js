3be02725510a7bf13a32ab3494753698860af64f
Rebecca Lemke <62661983+Bekalah@users.noreply.github.com>
2025-08-31 17:43:07 -0500

    theme.js

---
+++ assets/js/theme.js
@@ -0,0 +1,7 @@
+/* Forwarder: theme.js moved to /assets/js/engines/theme.js */
+(function () {
+  var s = document.createElement('script');
+  s.src = '/assets/js/engines/theme.js';
+  s.defer = true;
+  document.head.appendChild(s);
+})();
